1732268d5409e6dddc7ff6b4e1c9008cb091deee
Eric Kinsley <dt1993@gmail.com>
2020-04-19 17:32:14 -0500

    add firebase cdn

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,14 @@ var addName = $("#train-name");
 var addDestination = $("#train-destination");
 var addTime = $("#train-time").mask("00:00");
 var addFreq = $("#time-freq").mask("00");
+
+var config = {
+    apiKey: "AIzaSyDJqvfbZa280Qv7WVdpdr468ptOpsjoN7w",
+    authDomain: "uw-bootcamp-bb247.firebaseapp.com",
+    databaseURL: "https://uw-bootcamp-bb247.firebaseio.com",
+    projectId: "uw-bootcamp-bb247",
+    storageBucket: "uw-bootcamp-bb247.appspot.com", 
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
